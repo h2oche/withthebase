da850fb939ed39365ce39402316824f6199a1dd9
h2oche <h2oche22@gmail.com>
2015-09-13 16:03:28 +0000

    add login, logout, register

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -1,24 +1,30 @@
 class LeagueController < ApplicationController
   def dashboard
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
 
   def info
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
 
   def lineup
+    redirect_to '/users/sign_in' unless user_signed_in?
     @yet_drafted = true
   end
 
   def trade
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
 
   def draft
-    
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
   
   def autopick
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
   
   def result
+    redirect_to '/users/sign_in' unless user_signed_in?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
-    
-    has_many :rooms, through: :teams
-    has_many :teams, dependent: :destroy
-    
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+ 
 end
