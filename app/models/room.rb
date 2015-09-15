c96a920a7738639ac6f9728c742635bc364c2fd4
h2oche <h2oche22@gmail.com>
2015-09-15 10:55:47 +0000

    fix model

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,5 @@ class Room < ActiveRecord::Base
     has_many :users, through: :teams
     has_many :teams, dependent: :destroy
     has_many :games, dependent: :destroy
-    
+    has_one :draft
 end
